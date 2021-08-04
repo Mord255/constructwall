059c5181e3cd72ff9cb9e66f48d344bdb9f94566
Mord255 <jean.f.ponte.solis@gmail.com>
2021-08-03 23:55:52 -0500

    second commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -18,12 +18,12 @@
         <link rel="stylesheet" href="{{ asset('landing/fonts/icon-font/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('landing/fonts/typography-font/typo.css') }}">
         <link rel="stylesheet" href="{{ asset('landing/fonts/fontawesome-5/css/all.css') }}">
-        <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+        {{-- <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;500;700;900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet"> --}}
         <!-- Plugin'stylesheets  -->
         <link rel="stylesheet" href="{{ asset('landing/plugins/aos/aos.min.css') }}">
         <!-- Vendor stylesheets  -->
