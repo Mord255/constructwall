3f49a7ceaa3756a2263ca6919fc8d130a511bad4
Mord255 <jean.f.ponte.solis@gmail.com>
2021-08-13 00:03:29 -0500

    version_acomodando_v1

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,161 +1,185 @@
 
 @section('footer')
 
-	<!--Important link source from https://bootsnipp.com/snippets/ooa9M-->
-{{-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> --}}
-<!------ Include the above in your HEAD tag ---------->
+<footer class="footer" id="contacto">
+	{{-- <div class="container bottom_border">
+		<div class="row">
+		<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+		<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+		
+		<p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+		<p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
+		<p><i class="fa fa-phone"></i>  +91-9999878398  </p>
+		<p><i class="fa fa fa-envelope"></i> info@example.com  </p>
+		</div>
+		<div class=" col-sm-4 col-md  col-6 col">
+		<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+		
+		<ul class="footer_ul_amrc">
+		<li><a href="http://webenlance.com">Image Rectoucing</a></li>
+		<li><a href="http://webenlance.com">Clipping Path</a></li>
+		<li><a href="http://webenlance.com">Hollow Man Montage</a></li>
+		<li><a href="http://webenlance.com">Ebay & Amazon</a></li>
+		<li><a href="http://webenlance.com">Hair Masking/Clipping</a></li>
+		<li><a href="http://webenlance.com">Image Cropping</a></li>
+		</ul>
+		</div>
+		
+		<div class=" col-sm-4 col-md  col-6 col">
+		<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+		
+		<ul class="footer_ul_amrc">
+		<li><a href="http://webenlance.com">Remove Background</a></li>
+		<li><a href="http://webenlance.com">Shadows & Mirror Reflection</a></li>
+		<li><a href="http://webenlance.com">Logo Design</a></li>
+		<li><a href="http://webenlance.com">Vectorization</a></li>
+		<li><a href="http://webenlance.com">Hair Masking/Clipping</a></li>
+		<li><a href="http://webenlance.com">Image Cropping</a></li>
+		</ul>
+		
+		</div>
+		
+		
+		<div class=" col-sm-4 col-md  col-12 col">
+		<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+		
+		
+		<ul class="footer_ul2_amrc">
+		<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
+		<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
+		<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
+		</ul>
+		
+		</div>
+		</div>
+		</div> --}}
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-	<footer id="dk-footer" class="dk-footer " style="background-color: white">
-			<div class="container" id="contacto" >
-				<div class="row">
-					{{-- <div class="col-md-12 col-lg-4">
-						<div class="dk-footer-box-info">
-							<center>
-							<a href="#" class="footer-logo">
-								<img src="https://cdn.pixabay.com/photo/2016/11/07/13/04/yoga-1805784_960_720.png" alt="footer_logo" class="img-fluid">
-								<img src="{{ asset('landing/image/logo.png') }}" width="70%" class="img-fluid">
-							</a>
-							</center>
+	<div class="container">
+		<ul class="foote_bottom_ul_amrc">
+			<li><a href="#home">Inicio</a></li>
+			<li><a href="#servicios">Servicios</a></li>
+			<li><a href="#nosotros">Nosotros</a></li>
+			<li><a href="#contacto">Contactenos</a></li>
+		</ul>
+			<!--foote_bottom_ul_amrc ends here-->
+		
+
+		<ul class="social_footer_ul">
+			<li><a href="http://webenlance.com"><i class="fab fa-facebook-f"></i></a></li>
+			<li><a href="http://webenlance.com"><i class="fab fa-twitter"></i></a></li>
+			<li><a href="http://webenlance.com"><i class="fab fa-linkedin"></i></a></li>
+			<li><a href="http://webenlance.com"><i class="fab fa-instagram"></i></a></li>
+		</ul>
+	<!--social_footer_ul ends here-->
+	</div>
+	<div style="background-color: black" class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-align-center">
+					<div class=" text-white">
+						<span class="text-white">Copyright © 2021, All Right Reserved by ConstructWall</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</footer>
+
+
+
+
+
+{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+<div style="background-color: white">
+	<div class="container" id="contacto" >
+		<div class="row">
+			<div class="col-12">
+				<div class="text-align-center ">
 							
-							<br>
-							<div class="footer-social-link">
-								<h3>Nuestras Redes</h3>
-								<ul>
-									<li>
-										<a href="#">
-											<i class="fa fa-facebook"></i>
-										</a>
-									</li>
-									<li>
-										
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-google-plus"></i>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-linkedin"></i>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-whatsapp" style="background-color:green;"></i>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						
-					</div> --}}
-
-
-					<div class="col-md-12 col-lg-12">
-						{{-- <div class="row">
-							<div class="col-md-6">
-								<div class="contact-us">
-									<div class="contact-icon">
-										<i class="fa fa-map-o" aria-hidden="true"></i>
-									</div>
-									<div class="contact-info">
-										<h3>Jaipur India</h3>
-										<p>5353 Road Avenue</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="contact-us contact-us-last">
-									<div class="contact-icon">
-										<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
-									</div>
-									<div class="contact-info">
-										<h3>95 711 9 5353</h3>
-										<p>Give us a call</p>
-									</div>
-								</div>
-							</div>
-						</div> --}}
-						
-						<div class="row">
-							<div class="col-md-7 col-lg-7">
-								<div class="footer-widget">
-									<a href="#" class="footer-logo">
-										<img src="{{ asset('landing/image/logo1.jpg') }}" width="80%" class="img-fluid">
-									</a>
-								</div>
-							</div>
-							<div class="col-md-5 col-lg-5 ">
-								<div class="footer-widget footer-left-widget text-align-center ">
-										<div class="section-heading">
-											<h1 style="color:black;">Contactenos</h1>
-											<span class="animate-border border-black"></span>
-										</div>
-										<ul>
-											<li>
-												<a style="text-decoration: none" class="row" href="#" target="_blank">
-													<i style="color:red;font-size: 35px;" class="fas fa-map-marker-alt col-md-2"></i><h5 class="col-md-10 "> Direccion</h5> 
-												</a>
-											</li>
-											{{-- <li>
-												<a style="text-decoration: none" class="row" href="#" target="_blank">
-													<i style="color:black;" class="fas fa-phone fa-2x col-md-2"></i><h5 class="col-md-10 "> Numeros</h5> 
-												</a>
-											</li> --}}
-											<li>
-												<a style="text-decoration: none" class="row" href="#" target="_blank">
-													<i style="color:blue;font-size: 35px;" class="fab fa-facebook col-md-2"></i><h5 class="col-md-10 "> ConstructWall</h5> 
-												</a>
-											</li>
-										</ul>
-										<ul>
-											<li>
-												<a style="text-decoration: none" class="row" href="#" target="_blank">
-													<i style="color:green;font-size: 40px;" class="fab fa-whatsapp col-md-2"></i><h5 class="col-md-10 "> 972364645</h5> 
-												</a>
-											</li>
-											<li>
-												<a style="text-decoration: none" class="row" href="jean2000.jfps@gmail.com" target="_blank">
-													<i style="color:#CC1414;font-size: 35px;" class="fa fa-envelope  col-md-2"></i><h5 class="col-md-10 "> info@constructwall.com</h5> 
-												</a>
-											</li>
-										</ul>
-								</div>
-							</div>
-						</div>
+					<ul >
+						<li>
+							<a style="text-decoration: none"  href="#" target="_blank">
+								<i style="color:red;font-size: 35px;" class="fas fa-map-marker-alt "></i> Direccion
+							</a>
+						</li>
+						<li>
+							<a style="text-decoration: none"  href="#" target="_blank">
+								<i style="color:black;" class="fas fa-phone "></i> Numeros 
+							</a>
+						</li>
+						<li>
+							<a style="text-decoration: none"  href="#" target="_blank">
+								<i style="color:blue;font-size: 35px;" class="fab fa-facebook "></i> ConstructWall 
+							</a>
+						</li>
+						<li>
+							<a style="text-decoration: none" href="#" target="_blank">
+								<i style="color:green;font-size: 40px;" class="fab fa-whatsapp "></i> 972364645 
+							</a>
+						</li>
+						<li>
+							<a style="text-decoration: none"  href="jean2000.jfps@gmail.com" target="_blank">
+								<i style="color:#CC1414;font-size: 35px;" class="fa fa-envelope  "></i> info@constructwall.com
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div style="background-color: #015a68" class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-align-center">
+					<div class=" text-white">
+						<span class="text-white">Copyright © 2021, All Right Reserved</span>
 					</div>
 				</div>
 			</div>
-			<!-- copyright -->
-			<div style="background-color: #015a68" class="copyright">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-6 text-align-center">
-							<div class="copyright-menu">
-								<div class="copyright-menu text-white">
-									<span class="text-white">Copyright © 2021, All Right Reserved</span>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="copyright-menu">
-								<ul>
-									<li>
-										<a class="text-white" href="#">Volver al Inicio</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						{{-- <div class="col-md-12">
-							<div class="copyright-menu">
-								<span>Copyright © 2021, All Right Reserved</span>
-							</div>
-						</div> --}}
-					</div>
-				</div>
-			</div>
-	</footer>
+		</div>
+	</div>
+</div> --}}
+
 @endsection
+
+<style>
+
+/*footer*/
+/* .col_white_amrc { color:#FFF;} */
+footer { width:100%; background-color:#263238;  padding:10px 0px 0px 0px ;}
+/* .pt2 { padding-top:40px ; margin-bottom:20px ;}
+footer p { font-size:13px; color:#CCC; padding-bottom:0px; margin-bottom:8px;}
+.mb10 { padding-bottom:15px ;} */
+.footer_ul_amrc { margin:0px ; list-style-type:none ; font-size:14px; padding:0px 0px 10px 0px ; }
+.footer_ul_amrc li {padding:0px 0px 5px 0px;}
+.footer_ul_amrc li a{ color:#CCC;}
+.footer_ul_amrc li a:hover{ color:#fff; text-decoration:none;}
+.fleft { float:left;}
+/* .padding-right { padding-right:10px; } */
+
+/* .footer_ul2_amrc {margin:0px; list-style-type:none; padding:0px;}
+.footer_ul2_amrc li p { display:table; }
+.footer_ul2_amrc li a:hover { text-decoration:none;}
+.footer_ul2_amrc li i { margin-top:5px;} */
+
+.bottom_border { border-bottom:1px solid #323f45; padding-bottom:20px;}
+.foote_bottom_ul_amrc {
+	list-style-type:none;
+	padding:0px;
+	display:table;
+	margin-top: 10px;
+	margin-right: auto;
+	margin-bottom: 10px;
+	margin-left: auto;
+}
+.foote_bottom_ul_amrc li { display:inline;}
+.foote_bottom_ul_amrc li a { color:#999; margin:0 12px;}
+
+.social_footer_ul { display:table; margin:15px auto 0 auto; list-style-type:none;  }
+.social_footer_ul li { padding-left:20px; padding-top:10px; float:left; }
+.social_footer_ul li a { color:#CCC; border:1px solid #CCC; padding:8px;border-radius:50%;}
+.social_footer_ul li i {  width:20px; height:20px; text-align:center;}
+
+
+</style>
